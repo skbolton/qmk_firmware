bd061994e53474a239a36fc9460cfb31dc4837e8
Stephen Bolton <stephen@bitsonthemind.com>
2021-03-09 17:52:37 -0700

    (chore) Formatting in iris file

--- keyboards/crkbd/keymaps/skbolton/keymap.c
+++ keyboards/crkbd/keymaps/skbolton/keymap.c
@@ -14,7 +14,6 @@ extern uint8_t is_master;
 #define _ARROWS 4
 #define _WINDOW_PAD 5
 
-
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CAPS_ATTACK]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
   [TD_SLASHES]  = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_NUBS)
@@ -23,7 +22,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      TT(_FN),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,   KC_NUBS,   \
+      TT(_FN),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,   KC_BSLS,   \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       WINDOW, ARROWS,  KC_S,    ALTD,    CTRLF,   KC_G,                           KC_H,  CTRLJ,   ALTK,    KC_L,   KC_SCLN, KC_QUOT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
